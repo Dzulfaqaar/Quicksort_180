eb7ac50e283cbca42e622ed99b2b9f21a49c74df
dzulfaqaar <fikri6aul@gmail.com>
2023-03-31 10:54:10 +0700

    commit 1

--- Quicksort_180/Quicksort_180.cpp
+++ Quicksort_180/Quicksort_180.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
 using namespace std;
 
-//array of 
+//array of integrers to hold values
+int arr[20];
+int cmp_count = 0; //number of comparasion
+int mov_count = 0; //number of movement
+int n;
+
