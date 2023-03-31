739ca8772f98c066d9ab0ddc6e4f6656fc71ffd7
dzulfaqaar <fikri6aul@gmail.com>
2023-03-31 19:05:42 +0700

    commit 12

--- Quicksort_180/Quicksort_180.cpp
+++ Quicksort_180/Quicksort_180.cpp
@@ -36,33 +36,33 @@ void swap(int x, int y)
 	arr[x] = arr[y];
 	arr[y] = temp;
 }
-void q_short(int low, int high)
-{
+
+void q_short(int low, int high) {
 	int pivot, i, j;
 	if (low > high) //langkah 1
 		return;
 
 
-//partition the list into two part
-//one containing element less that or equal to pivot
-//outher containing element greather than pivot
-i=low + 1; //langkah 3
-j = high; //langlah 4
-pivot = arr[low]; //langkah 2
+	//partition the list into two part
+	//one containing element less that or equal to pivot
+	//outher containing element greather than pivot
+	i = low + 1; //langkah 3
+	j = high; //langlah 4
+	pivot = arr[low]; //langkah 2
 
-while (i <= j) //langkah 10
-{
-	//search for an element greater than pivot
-	while ((arr[i] <= pivot) && (i <= high))//langkah 5
-	{
+
+	while (i <= j) //langkah 10
+	{ 
+		//search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))//langkah 5
+		{
 		i++;//langkah6
 		cmp_count++;
-	}
-	cmp_count++;
-	//search for an element less thanor equal to pivot
-	while ((arr[j] > pivot) && (j >= low))//langkah 7
-	{
-		j--;//langkah 8
+		}
+		cmp_count++;
+		//search for an element less thanor equal to pivot
+		while ((arr[j] > pivot) && (j >= low))//langkah 7
+			j--;//langkah 8
 		cmp_count++;
 	}
 	cmp_count++;
@@ -78,7 +78,8 @@ while (i <= j) //langkah 10
 	//sort the list on the right of pivot using quick short
 	q_short(j + 1, high);//langkah 13
 }
-void display () {
+
+void display(){
 	cout << "\n================" << endl;
 	cout << "\n=sorted array" << endl;
 	cout << "\n================" << endl;
@@ -86,7 +87,24 @@ void display () {
 	for (int i = 0; i < n;i++) {
 		cout << arr[i] << " ";
 	}
-	cout << "\n\number"
+	cout << "\n\number of comparison:" << cmp_count << endl;
+	cout << "Number of data movement:" << mov_count << endl;
+}
+
+int main(){
+	input();
+	//dort the array using quick sort
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
+
+
+
+
+
 
 
 			
